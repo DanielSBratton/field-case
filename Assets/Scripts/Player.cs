03e3ad7a3662a5332acd968bcb3dd6f5c00bb372
DanielSBratton <dsb00015@mix.wvu.edu>
2025-10-05 21:17:32 -0400

    CoreDemo Scene
    Built a demo scene to work out core mechanic of 'sound' based enemy tracking.
    -Enemy wanders without noise.
    -Movement of player on noisy tile attracts enemy.
    -Enemy loses interest if stopped.
    -Bonus: limited light/visibility.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,113 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Collider2D coll;
+
+    Collider2D noiseSource;
+    public Enemy enemy;
+
+    Vector2 direction;
+    public int noise = 0;
+
+    
+
+    // Get Components
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
+    }
+
+    // Analyze input, move player, and check for noise sources
+    void FixedUpdate()
+    {
+        // Movement handling
+        InputRead();
+        rb.MovePosition(rb.position + (direction * 0.5f * Time.deltaTime));
+
+        // Noise handling
+        if (direction != Vector2.zero)
+        {
+            // Check for overlap with noisy objects
+            List<Collider2D> results = new List<Collider2D>();
+            int hits = coll.Overlap(new ContactFilter2D
+            {
+                layerMask = LayerMask.GetMask("Noisy"),
+                useLayerMask = true,
+                useTriggers = true
+            }, results);
+
+            noise = 0; // Reset noise
+
+            // Determine noise level based on tag of object overlapped
+            foreach (var hit in results)
+            {
+                switch (hit.gameObject.tag)
+                {
+                    case "Alert1":
+                        noise = 5;
+                        break;
+                    case "Alert2":
+                        noise = 6;
+                        break;
+                    case "Alert3":
+                        noise = 7;
+                        break;
+                    default:
+                        noise = 0;
+                        break;
+                }
+            }
+            enemy.Noise(rb.position, noise); // Notify enemy of noise
+        }
+    }
+
+    // Read player input and adjust direction
+    void InputRead()
+    {
+        if (Keyboard.current[Key.W].IsActuated() || Keyboard.current[Key.UpArrow].IsActuated())
+        {
+            if (direction.y < 5)
+            {
+                direction.y += 0.5f;
+            }
+        }
+        if (Keyboard.current[Key.S].IsActuated() || Keyboard.current[Key.DownArrow].IsActuated())
+        {
+            if (direction.y > -5)
+            {
+                direction.y -= 0.5f;
+            }
+        }
+
+        if (Keyboard.current[Key.A].IsActuated() || Keyboard.current[Key.LeftArrow].IsActuated())
+        {
+            if (direction.x > -5)
+            {
+                direction.x -= 0.5f;
+            }
+        }
+        if (Keyboard.current[Key.D].IsActuated() || Keyboard.current[Key.RightArrow].IsActuated())
+        {
+            if (direction.x < 5)
+            {
+                direction.x += 0.5f;
+            }
+        }
+
+        if (Mathf.Abs(direction.x) > 0)
+        {
+            direction.x -= 0.25f * Mathf.Sign(direction.x);
+        }
+        if (Mathf.Abs(direction.y) > 0)
+        {
+            direction.y -= 0.25f * Mathf.Sign(direction.y);
+        }
+    }
+}
